263a131c73b783326fb7d28c7f90fe53c5bb83a8
Gayathri <gaya.0408@gmail.com>
2014-11-22 01:58:50 -0800

    DS : Binary Search Trees

---
+++ Tree.java
@@ -0,0 +1,58 @@
+public class Tree {
+
+	private Tree left;
+	private Tree right;
+	private int data;
+
+	public Tree(int data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+
+	public void insert( Tree node, int x) {
+
+		if( x < node.data ) {
+			if(node.left != null) {
+				insert(node.left, x);
+			} else {
+				node.left = new Tree(x);
+			}
+		}
+		else {
+			if(node.right != null) {
+				insert(node.right, x);
+			} else {
+				node.right = new Tree(x);
+			}
+		}
+
+	}
+
+	/*
+	Inorder Traversal 
+	*/
+	public void traversal(Tree node) {
+		if(node != null) {
+			traversal(node.left);
+			System.out.println(" node.data:: " + node.data);
+			traversal(node.right);
+		}
+	}
+
+	public static void main(String[] args) {
+		Tree root = new Tree(10);
+		Tree node = root;
+		root.insert(node, 5);
+		root.insert(root, 15);
+		root.insert(root, 1);
+		root.insert(root, 7);
+		root.insert(root, 6);
+
+		root.search(root, 25);
+
+		root.traversal(root);
+		
+	}
+
+}
